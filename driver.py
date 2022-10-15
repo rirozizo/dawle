b0776c61c9a58df650bcb14d1039748f6b651c06
rirozizo <rawadzahreddine32@gmail.com>
2022-10-15 23:47:52 +0300

    Don't send a notification as soon as the script runs

--- driver.py
+++ driver.py
@@ -1,53 +1,59 @@
 import time, os, requests
 
 hostname = "192.168.18.155"
-global response
-global number_of_attempts
-global kahraba
-global first_run
+response = 0
+number_of_attempts = 0
+kahraba = True
 first_run = True
 URL = "IFTT WEBHOOK FOR DAWLE HERE"
 no_URL = "IFTT WEBHOOK FOR NO DAWLE HERE"
 
+
 def n2ata3it_aw_ba3ed():
+    global first_run
     number_of_attempts = 0
     while number_of_attempts < 10:
-      try:
-        response= os.system("ping -c 1 " + hostname)
-      except Exception as e:
-        print(e)
+        try:
+            response = os.system("ping -c 1 " + hostname)
+        except Exception as e:
+            print(e)
+            time.sleep(5)
+            break
+        if response != 0:
+            number_of_attempts += 1
+            continue
+        number_of_attempts = 0
+        first_run = False
+        print(str(response))
+        print("Host is up")
         time.sleep(5)
-        break
-      if response != 0:
-        number_of_attempts += 1
-        continue
-      number_of_attempts = 0
-      print(str(response))
-      print("Host is up")
-      time.sleep(5)
     n2ata3it()
 
+
 def n2ata3it():
+    global first_run
     kahraba = False
     print("Host is down")
-    if first_run == False:
+    print("=============================================")
+    if not first_run:
+        print("ra7it el dawle IFTTT WebHook")
         requests.get(no_URL)
-    if first_run == True:
+    else:
         first_run = False
-#    requests.get(no_URL)
 
-    while kahraba == False:
-      try:
-        response= os.system("ping -c 1 " + hostname)
-      except:
-#        print("ERROR: Unreachable")
-        time.sleep(5)
-        break
-      if response == 0:
-        kahraba = True
-        print("Host is up")
-        print("Sending IFTTT WebHook")
-        requests.get(URL)
+    while not kahraba:
+        try:
+            response = os.system("ping -c 1 " + hostname)
+        except:
+            #        print("ERROR: Unreachable")
+            time.sleep(5)
+            break
+        if response == 0:
+            kahraba = True
+            print("Host is up")
+            print("Sending IFTTT WebHook")
+            requests.get(URL)
+
 
 while True:
-  n2ata3it_aw_ba3ed()
+    n2ata3it_aw_ba3ed()
